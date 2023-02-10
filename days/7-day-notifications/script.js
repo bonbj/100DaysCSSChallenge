3a4b862ee3aadbb2179f442d46a376660ef574b9
joao victor <jaotipereira@gmail.com>
2023-02-10 20:43:56 -0300

    feat: day 7 done

--- days/7-day-notifications/script.js
+++ days/7-day-notifications/script.js
@@ -1,4 +1,19 @@
-$('.board-header-btn-search').click(()=> {
-    console.log('hello')
-    $('.board-header-search').toggleClass('board-header-search-active');
-}); 
+let isCardleft = false;
+
+
+$('.board-header-btn-search').click(() => {
+    if(!isCardleft) {
+        $('.board-header-search').toggleClass('board-header-search-active');
+    }
+});
+
+
+$('.board-header-btn-menu').click(() => {
+    if(!isCardleft) {
+        $(".index1").animate({left: '190px'});
+        isCardleft = true;
+    } else {
+        $(".index1").animate({left: '50px'});
+        isCardleft = false;
+    }
+})
